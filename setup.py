d015870d3d374001f37c9b83c395400be42793f2
Juan.ChaconLeon <juan.chacon@teck.com>
2022-10-03 07:48:42 -0700

    Fixing package generation

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="opt_sugar",
-    version="0.1",
+    version="0.0.2",
     author="Juan Chacon",
     author_email="juandados@gmail.com",
-    packages=find_packages("opt_sugar"),
-    package_dir={"": "opt_sugar"},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     url="https://github.com/juandados/opt-sugar",
     keywords="optimization operations mathematical programming",
     install_requires=[
